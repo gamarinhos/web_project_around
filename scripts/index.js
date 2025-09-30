e07a3f46dd23e449e9c5def201f81bbd544e1f69
Gamarinho <gamarinho.email@gmail.com>
2025-09-30 00:33:24 -0300

    sprint-8 first commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,20 @@
 const popupProfile = document.querySelector('.popup-profile');
 const popupProfileForm = popupProfile.querySelector('.popup-profile__form');
-const popupProfileOpenButton = document.querySelector('.profile__edit-button');
-const popupProfileCloseButton = popupProfile.querySelector('.popup-profile__close-button');
 const popupProfileName = popupProfileForm.querySelector('#profile-name');
 const popupProfileJob = popupProfileForm.querySelector('#profile-job');
-const profileName = document.querySelector('.profile__name');
-const profileJob = document.querySelector('.profile__job');
+const displayName = document.querySelector('.profile__name');
+const displayJob = document.querySelector('.profile__job');
 
 function togglePopupProfile() {
     popupProfile.classList.toggle('popup-profile_active');
-
-    popupProfileName.value = profileName.textContent;
-    popupProfileJob.value = profileJob.textContent;
-
+    
+    popupProfileName.value = displayName.textContent;
+    popupProfileJob.value = displayJob.textContent;
+    
 }
+
+const popupProfileOpenButton = document.querySelector('.profile__edit-button');
+const popupProfileCloseButton = popupProfile.querySelector('.popup-profile__close-button');
 
 popupProfileOpenButton.addEventListener('click', togglePopupProfile);
 popupProfileCloseButton.addEventListener('click', togglePopupProfile);
@@ -22,9 +23,8 @@ popupProfileCloseButton.addEventListener('click', togglePopupProfile);
 popupProfileForm.addEventListener('submit', function(evt){
     evt.preventDefault();
 
-    profileName.textContent = popupProfileName.value;
-    profileJob.textContent = popupProfileJob.value;
+    displayName.textContent = popupProfileName.value;
+    displayJob.textContent = popupProfileJob.value;
 
     popupProfile.classList.remove('popup-profile_active');
 });
-
