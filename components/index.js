a2c4d607f8732e9b8483e5dcfadadd37e086dcfa
Gamarinho <gamarinho.email@gmail.com>
2025-12-27 16:43:29 -0300

    Add index.js: Create a central export file for components

---
+++ components/index.js
@@ -0,0 +1,6 @@
+export { Popup } from './Popup.js';
+export { PopupWithImage } from './PopupWithImage.js';
+export { PopupWithForm } from './PopupWithForm.js';
+export { Card } from './Card.js';
+export { Section } from './Section.js';
+export { FormValidator } from './FormValidator.js';
