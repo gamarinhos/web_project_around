ed821474c5dc87fc9defb87d1d16448bbdd524fd
Gamarinho <gamarinho.email@gmail.com>
2025-12-27 16:42:44 -0300

    Add Popup class: Implement a new Popup component for managing modal behavior

---
+++ components/Popup.js
@@ -0,0 +1,41 @@
+export class Popup {
+  constructor(selector) {
+    this._popup = document.querySelector(selector);
+    this._closeButton = this._popup.querySelector(`.${this._class}__close-button`);
+    this._class = this._popup.classList[0];
+
+    this._bindMethods('_handleOverlayClick', '_handleEscapeKey');
+  }
+
+  _bindMethods(...methods) {
+    methods.forEach((method) => {
+      this[method] = this[method].bind(this);
+    });
+  }
+
+  open() {
+    this._popup.clasList.add(`${this._class}_active`);
+    this._popup.addEventListener('click', this._handleClickClose);
+    document.addEventListener('keydown', this._handleEscClose);
+  }
+
+  close() {
+    this._popup.classList.remove(`${this._class}_active`);
+    this._popup.removeEventListener('click', this._handleClickClose);
+    document.removeEventListener('keydown', this._handleEscClose);
+  }
+
+  _handleClickClose(event) {
+    const isOverlay = event.target === this._popup || event.target === this._closeButton;
+
+    if (isOverlay) {
+      this.close();
+    }
+  }
+
+  _handleEscClose(event) {
+    if (event.key === 'Escape') {
+      this.close();
+    }
+  }
+}
