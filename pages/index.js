1bcef6461c107bfbc92a3b9e141c765964676921
Gamarinho <gamarinho.email@gmail.com>
2025-12-29 23:59:02 -0300

    Refactor index.js: Consolidate popup functionality and improve form handling for profile and card creation. Remove unused PopupWithForm and streamline event listeners for better clarity and maintainability.

--- pages/index.js
+++ pages/index.js
@@ -1,142 +1,94 @@
+//**IMPORTANTE!
+//
+// 1. Utilize um navegador baseado em Chromium para
+// visualizar o botão de fechamento do popup. Caso
+// contrário, isso não impede o fechamento do popup
+// ao clicar no overlay ou precionando 'Esc', mas
+// apenas a visualização do "X".
+//
+// 2. Não entendi como eu deveria usar o 'PopupWithForm'
+// nem para quê ele serviria já que 'index.js' é quem
+// orquestra a relação entre as classes. Fiz com que o
+// submitter do 'FormValidator' ficasse responsável por
+// fechar o popup após o envio do forumlário.
+// */
+
 import {
   Popup,
   PopupWithImage,
-  PopupWithForm,
   Card,
   Section,
   FormValidator,
+  UserInfo,
 } from '../components/index.js';
 
-//// Popup form that update profile info ////
-(function profilePopup() {
-  const profileForm = new FormPopup({
-    form: document.forms.profile,
-  });
-  profileForm.enableValidation();
-  const { name: nameInput, job: jobInput } = profileForm.inputs;
-  const profileSection = {
-    name: document.querySelector('.profile__name'),
-    job: document.querySelector('.profile__job'),
-  };
-
-  function fillProfileFormInputs() {
-    nameInput.value = profileSection.name.textContent;
-    jobInput.value = profileSection.job.textContent;
-  }
-
-  // Open popup event
-  const openButton = document.querySelector('.profile__edit-button');
-
-  openButton.addEventListener('click', () => {
-    fillProfileFormInputs();
-    profileForm._hasInvalidInput();
-    profileForm._toggleButtonState();
-    profileForm.openPopup();
-  });
-
-  // Form subimission event
-  function updateProfileInfo(data = { name, job }) {
-    profileSection.name.textContent = data.name;
-    profileSection.job.textContent = data.job;
-  }
-
-  profileForm.form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    if (profileForm._hasInvalidInput()) {
-      return; // stop execution
-    }
-
-    const values = {
-      name: nameInput.value,
-      job: jobInput.value,
-    };
-
-    updateProfileInfo(values);
-    profileForm.closePopup();
-  });
-})();
+import { selectors, initialCards } from '../utils/constants.js';
 
 //// Popup form that add cards ////
-(function newCardPopup() {
-  //// Page initial cards ////
-  const initialCards = [
-    {
-      title: 'Lago di Braies',
-      link: './images/3dbe7008cf5622c19034ae50ddc30348e49c2ac6.webp',
-      alt: 'barcos atracados em um lago com montanhas ao fundo',
-    },
-    {
-      title: 'Lago Louise',
-      link: './images/221cfcb6f665950ee520ab6c9cc460dd246710c4.webp',
-      alt: 'montanhas com lago cristalino ciano cristalino refletindo as montanhas e o céu claro',
-    },
-    {
-      title: 'Parque Nacional da Vanoise ',
-      link: './images/906f6bf6cf25eb556dd75f223d72ade0d4bccb93.webp',
-      alt: 'paisagem montanhosa com lago refletindo as montanhas e o céu',
-    },
-    {
-      title: 'Vale de Yosemite',
-      link: './images/4678c5ba8916c0b28b3e15fdbea3fa7a96fe0cd5.webp',
-      alt: 'rio cercado por árvores com montanhas ao fundo',
-    },
-    {
-      title: 'Montanhas Carecas',
-      link: './images/5777c3a01f7043c8a872fcb759976babd53a3d4e.webp',
-      alt: 'por do sol sobre montanhas',
-    },
-    {
-      title: 'Latemar',
-      link: './images/de4f23b1370b713bab6281cf68c1f6d4782362bd.webp',
-      alt: 'montanhas sob um céu noturno estrelado',
-    },
-    {
-      title: 'Error example',
-      link: 'https://broken-url.com',
-      alt: 'Error example',
-    },
-  ];
+(function cards() {
+  const imagePopup = new PopupWithImage(selectors.popups.imagePopup);
 
-  const cardSection = new Section('.content__section-cards', {
+  const cardSection = new Section(selectors.sections.cards, {
     data: initialCards,
-    renderer(data) {
-      const card = new Card(data);
-      const cardElement = card.getCardElement('#card-template');
-      this.setItem(cardElement);
-    },
+    renderer: addNewCard,
   });
+
+  function addNewCard(data) {
+    data.cardClickHandler = ({ title, link }) => imagePopup.open(title, link);
+
+    const card = new Card(data);
+    const cardElement = card.getCardElement(selectors.templates.card);
+    cardSection.addItem(cardElement);
+  }
 
   cardSection.renderer();
 
-  const newCardForm = new FormPopup({
+  const newCardPopup = new Popup(selectors.popups.newCard);
+
+  const newCardForm = new FormValidator({
     form: document.forms.newCard,
+    submitter() {
+      const cardValues = newCardForm.getInputsValues();
+      cardValues.cardClickHandler = ({ title, link }) => imagePopup.open(title, link);
+      const card = new Card(cardValues);
+      const cardElement = card.getCardElement(selectors.templates.card);
+      cardSection.addItem(cardElement);
+
+      newCardPopup.close();
+    },
   });
 
-  newCardForm.enableValidation();
+  // Open popup event
+  const openButton = document.querySelector(selectors.profile.addButton);
+  openButton.addEventListener('click', () => {
+    newCardPopup.open();
+  });
+})();
 
-  const openButton = document.querySelector('.profile__add-button');
-  openButton.addEventListener('click', () => newCardForm.openPopup());
+//// Popup form that update profile info ////
+(function profilePopup() {
+  const profilePopup = new Popup(selectors.popups.profile);
 
-  newCardForm.form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    if (newCardForm._hasInvalidInput()) {
-      return; // stop execution
-    }
-
-    const values = {
-      title: newCardForm.inputs.title.value,
-      link: newCardForm.inputs.link.value,
-    };
-
-    addNewCard(values);
-    newCardForm.closePopup();
-    newCardForm.form.reset();
+  const profileInfo = new UserInfo({
+    name: document.querySelector(selectors.profile.name),
+    job: document.querySelector(selectors.profile.job),
   });
 
-  function addNewCard(data = { title, link, alt }) /* Object hint */ {
-    const card = new Card(data);
-    cardSection.append(card.getCardElement('#card-template'));
-  }
+  const profileForm = new FormValidator({
+    form: document.forms.profile,
+    submitter() {
+      const values = profileForm.getInputsValues();
+      profileInfo.setUserInfo(values);
+      profilePopup.close();
+    },
+  });
+
+  // Open popup event
+  const openButton = document.querySelector(selectors.profile.editButton);
+  openButton.addEventListener('click', () => {
+    const values = profileInfo.getUserInfo();
+    profileForm.prefillInputs(values);
+    profileForm.toggleButtonState();
+    profilePopup.open();
+  });
 })();
