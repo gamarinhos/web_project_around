1c77e076b8fe976e22f81db978e44215c49aadb1
Gamarinho <gamarinho.email@gmail.com>
2025-12-30 00:18:14 -0300

    Remove unused exports from index.js

--- components/index.js
+++ components/index.js
@@ -1,8 +1,6 @@
 export { Popup } from './Popup.js';
 export { PopupWithImage } from './PopupWithImage.js';
-export { PopupWithForm } from './PopupWithForm.js';
 export { Card } from './Card.js';
 export { Section } from './Section.js';
 export { FormValidator } from './FormValidator.js';
-export { Button } from './Button.js';
 export { UserInfo } from './UserInfo.js';
