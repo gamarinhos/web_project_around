15c6f6e5aa8a9081a36ec529fd38f04fdae1d87b
Gamarinho <gamarinho.email@gmail.com>
2025-12-29 23:56:32 -0300

    Add Button and UserInfo exports: Include Button and UserInfo components in the central index.js file for improved accessibility.

--- components/index.js
+++ components/index.js
@@ -4,3 +4,5 @@ export { PopupWithForm } from './PopupWithForm.js';
 export { Card } from './Card.js';
 export { Section } from './Section.js';
 export { FormValidator } from './FormValidator.js';
+export { Button } from './Button.js';
+export { UserInfo } from './UserInfo.js';
