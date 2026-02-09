cc654acc96d13b853e009191e5b2c79d2d445e4d
Gamarinho <gamarinho.email@gmail.com>
2026-02-09 15:53:16 -0300

    Update constants.js: Add new section for card selectors, remove outdated popup selectors, and replace initial card data with updated names and links for various mountain ranges.

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,7 @@
 export const selectors = {
+  sections: {
+    cards: '.content__section-cards',
+  },
   profile: {
     name: '.profile__name',
     about: '.profile__about',
@@ -7,14 +10,6 @@ export const selectors = {
   },
   templates: {
     card: '#card-template',
-  },
-  popups: {
-    imagePopup: '#image-popup',
-    profile: '#profile',
-    newCard: '#newCard',
-  },
-  sections: {
-    cards: '.content__section-cards',
   },
   year: '#year',
 };
@@ -29,31 +24,31 @@ export const tripleten = {
 
 export const initialCards = [
   {
-    name: 'Lago di Braies',
-    link: './images/3dbe7008cf5622c19034ae50ddc30348e49c2ac6.webp',
+    name: 'Alaska Range',
+    link: 'https://www.travelandleisure.com/thmb/aw1hyc5dIRbZE_Hjr7vkrjB_P_k=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/TAL-alaska-range-USMNTNSTMOG0623-cddd55a6d03f4935ab43a97da82fb0ae.jpg',
   },
   {
-    name: 'Lago Louise',
-    link: './images/221cfcb6f665950ee520ab6c9cc460dd246710c4.webp',
+    name: 'Great Smoky Mountains',
+    link: 'https://www.travelandleisure.com/thmb/Kgb4PoS05a66J3R9qq1CFeGq8f0=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/great-smoky-mountains-USMNTNS0720-85421f95ebc342fabf4b17fcede2a7ff.jpg',
   },
   {
-    name: 'Parque Nacional da Vanoise ',
-    link: './images/906f6bf6cf25eb556dd75f223d72ade0d4bccb93.webp',
+    name: 'Teton Range',
+    link: 'https://www.travelandleisure.com/thmb/GmekR2hjot6B2mWovmI4J192nSk=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/TAL-grand-teton-USMNTNSIPOG0823-2538d183b9094e3fb59dd5e54bbe791c.jpg',
   },
   {
-    name: 'Vale de Yosemite',
-    link: './images/4678c5ba8916c0b28b3e15fdbea3fa7a96fe0cd5.webp',
+    name: 'Uinta Montains',
+    link: 'https://www.travelandleisure.com/thmb/bJMyN_3JBEi2SuSnexOoUqO9hOA=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/uinta-mountains-USMNTNS0720-143e32955e1543c2a0674a501e280f88.jpg',
   },
   {
-    name: 'Montanhas Carecas',
-    link: './images/5777c3a01f7043c8a872fcb759976babd53a3d4e.webp',
+    name: 'Sawtooth Range',
+    link: 'https://www.travelandleisure.com/thmb/i5cVcgXQ1Cy2BFDif0HT6it7W9A=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/sawtooth-mountains-USMNTNS0720-2ad167c1f008453a8d362efe5a5c302a.jpg',
   },
   {
-    name: 'Latemar',
-    link: './images/de4f23b1370b713bab6281cf68c1f6d4782362bd.webp',
+    name: 'Bighorn Montains',
+    link: 'https://www.travelandleisure.com/thmb/8uYE4jQ_y1o2629zU9S7cUKCj6s=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/bighorn-mountains-wyoming-USMNTNS0720-cb9457943a8d491b9c1eaae0bd3e8c1f.jpg',
   },
   {
-    name: 'Error example',
-    link: 'https://broken-url.com',
+    name: 'Elias National Park',
+    link: 'https://www.travelandleisure.com/thmb/g727-mPNmnsphYdSQ2_7PW74KvQ=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/wrangell-st-elias-national-park-alaska-BEAUTYSTS0522-aa733bbb6af64cd4b64d2672a30bc8ac.jpg',
   },
 ];
